1f5e9f5ec0bf5ad49cb428575a5c6ca37dfa1325
Leon Rosenplänter <137449681+lrosenplaenter@users.noreply.github.com>
2025-03-14 22:21:50 +0100

    finanlize html gen

--- .github/scripts/manage.js
+++ .github/scripts/manage.js
@@ -4,6 +4,8 @@ const readline = require("readline");
 const fs = require("fs");
 const path = require("path");
 const { createCanvas } = require('canvas');
+const { HtmlValidate } = require('html-validate');
+const prettify = require('html-prettify');
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -12,6 +14,7 @@ const rl = readline.createInterface({
 
 const DATA_PATH = path.join(__dirname, "../../sources");
 const STAGE_PATH = path.join(__dirname, "../../sources/to_integrate");
+const WEBSITE_PATH = path.join(__dirname, "../../website");
 const DATABASE = "data.json"
 
 /*** Functions ***/
@@ -27,7 +30,7 @@ async function main() {
     if (userChoice === "Stage new treasure") {
         await stage_new_treasure();
     } else if (userChoice === "Integrate new treasures") {
-        integrate_new_treasures();
+        await integrate_new_treasures();
     } else if (userChoice === "Database maintenance") {
         console.log("Not yet implemented! :(")
         // TODO
@@ -122,7 +125,7 @@ async function saveNewEntry(entry) {
 }
 
 /* integrate new */
-function integrate_new_treasures () {
+async function integrate_new_treasures () {
     let data = []; // to hold the content that will be stores as data.json
 
     // getting new treasures + add "new" flag + add timestamp
@@ -173,7 +176,7 @@ function integrate_new_treasures () {
     // clear to_integrate dir from .json files
     deleteStagedJsonFiles(STAGE_PATH, TreasuresToIntegrate);
 
-    generateHtml(data);
+    await generateHtml(data);
 }  
 
 // get data from title.json files in ./sources/to_integrate
@@ -303,26 +306,203 @@ function deleteStagedJsonFiles(directory, entries) {
     });
 }
 
-function generateHtml (data) {
-    console.log(data)
-    var entries_html = [];
+// generate the sites HTML
+async function generateHtml (data) {
+    console.log("Generating HTML...")
 
+    // generate data structure
+    var html = "";
+    var data_by_topics = getTopicsAndSubtopics()
+    //Format: {topic: TOPIC, entries: [], subtopics: [{subtopic: SUBTOPIC, entries: []},{subtopic: SUBTOPIC, entries: []}]}
+
+    // Gernate the HTML for each entry + place in topic-ordered data struct
     for (var i in data) {
-        var key = Object.keys(data[i])[0] // get existing entries
+        // generate entry card html & get topics for each entry
+        var entry_html = null;
+        var entry_topics = null;
+        var key = Object.keys(data[i])[0] // get Key = existing_entry or new_entry 
+
         if (key == "existing_entry") {
-            entries_html.push(generateEntryHtml(data[i].existing_entry, false))   
-        } else if (key == "new_entry") {    
-            entries_html.push(generateEntryHtml(data[i].new_entry, true)) 
+            console.log(`... ${data[i].existing_entry.title}`)
+            entry_html = generateEntryHtml(data[i].existing_entry, false)
+            entry_topics = data[i].existing_entry.data.topics
+        } else if (key == "new_entry") {
+            console.log(`... ${data[i].new_entry.title}`)    
+            entry_html = generateEntryHtml(data[i].new_entry, true)
+            entry_topics = data[i].new_entry.data.topics
+        }
+
+        // push entries to the right topic
+        for (var j in entry_topics) {
+            // check if subtopics are set
+            if(entry_topics[j].subtopics.length >= 1) { // there is at least one subtopic to be pushed to
+                for (var k in entry_topics[j].subtopics) {
+                    var searchTopic = entry_topics[j].topic
+                    var searchSubtopic = entry_topics[j].subtopics[k]
+                    var topicIndex = data_by_topics.findIndex(obj => obj.topic == searchTopic)
+                    var subtopicIndex = data_by_topics[topicIndex].subtopics[0].findIndex(obj => obj.subtopic == searchSubtopic)
+                    data_by_topics[topicIndex].subtopics[0][subtopicIndex].entries.push(entry_html)
+                }
+            } else { // no subtopics are found, therefore only push to "main"-topic
+                var searchTopic =  entry_topics[j].topic
+                var topicIndex = data_by_topics.findIndex(obj => obj.topic == searchTopic)
+                data_by_topics[topicIndex].entries.push(entry_html)
+            }
         }
     }
+    
+    //html prototypes
+    const html_pre = `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <title>The Internets Treasure Chest</title>
+            <link href="public_modules/bootstrap/bootstrap.min.css" rel="stylesheet">
+            <link href="public_modules/bootstrap-icons/bootstrap-icons.min.css" rel="stylesheet">
+            <script src="public_modules/bootstrap/bootstrap.bundle.min.js"></script>
+        </head>
+        <body data-bs-spy="scroll" data-bs-target="#navbar-top" data-bs-offset="0" tabindex="0">
+            <header class="fixed-top">
+                <nav id="navbar-top" class="navbar navbar-expand-lg navbar-light bg-light border-bottom border-secondary">
+                    <div class="container">
+                        <a class="navbar-brand" href="#">
+                            <img src="ressources/TreasureChest.png" alt="Logo" width="60" height="43" class="d-inline-block">
+                            The Internets Treasure Chest
+                        </a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarNav">
+                            <ul class="ms-auto navbar-nav">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#treasurechest">Treasure Chest</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#about">About</a>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link" href="#more">More</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+            </header>
+            <main style="margin-top: 70px;"> <!--adjust to height of Navbar-->
+                <div class="container">
+                    <section id="treasurechest" class="pt-3 pb-5">
+                        <h2 class="pb-5 visually-hidden">Treasure Chest</h2> <!--Set to be invisible, but still included to not break h hierarchy-->
+                        <div id="topics-container">
+                            <!--<h3>Topics</h3>-->
+                                <!--Main Topics-->`;
+    const html_post = 
+                `</div>
+                        <!--<div id="Types-container">
+                        </div>
+                        <div id="Tags-container">
+                        </div>
+                        <div id="Flags-container">
+                        </div>-->
+                    </section>
+                    <section id="about" class="pb-5">
+                        <h2>About</h2>
+                        <p>
+                            I started keeping this list because my bookmarks in the browser have simply become too cluttered and I have often
+                            received comments like ‘How do you always find such cool videos/websites?’. Honestly, I can't really answer this 
+                            question, but for everyone else: Here you go, a list of all my favourite free, interesting
+                            and entertaining websites. 	
+                        </p>
+                        <p><b>Please note the information below:</b></p>
+                        <ul>
+                            <li>This is a private hobby project. I do not claim to list *every* great resource here.</li>
+                            <li>I cannot guarantee that all links will work forever, I cannot vouch for the safety, usefulness, timeliness, 
+                                etc. of the linked resources.</li>
+                            <li>There is no advertising here, and no content is sponsored. All content has been selected because I personally 
+                                like it, find it useful or entertaining. The content is therefore naturally based on my personal interests. 
+                                But maybe there's something here that you'll like.</li>
+                            <li>That is why you will also find content that I have created myself. These are transparently flagged as 
+                                “shameless-self-plug”.</li>
+                        </ul>
+                    </section>
+                    <section id="more" class="pb-5">
+                        <h2>More Stuff</h2>
+                        <p>(*・‿・)ノ⌒*:･ﾟ✧ There's more planned for the future:  filters, a search function, a newsletter, a feature that lets you suggest a new treasure, ... ✧ﾟ･:⌒ヽ(・‿・*)</p>
+                    </section>
+                </div>
+            </main>
+            <footer class="text-muted py-5">
+                <div class="container">
+                    <a href="#" class="position-fixed bottom-0 end-0 mb-3 me-3 button-primary" style="font-size: 2rem;" aria-label="Back To Top Button">
+                        <i class="bi bi-arrow-up-circle-fill"></i>
+                    </a>
+                </div>
+                <div class="container">
+                    <p class="text-center">Copyright (c) 2025 Leon Rosenplaenter. Assembled using node.js, <a href="https://github.com/twbs/bootstrap" target="_blank">Bootstrap</a> and <a href="https://github.com/twbs/icons" target="_blank">Bootstrap Icons</a>. Hosted on GitHub.</p>
+                </div>
+            </footer>
+            <!--<script src="index.js"></script>-->
+        </body>
+        </html>`;
 
-    //console.log(entries_html)
+    // compose the full html page
+    html += html_pre; // upper part of the page, static
+    for (var i in data_by_topics) { // main section of the page, dyn. generated
+        // main-topic (upper)
+        html += 
+        `<ul class="list-group">
+            <li class="list-group-item d-flex justify-content-between align-items-start border-dark">
+                <div id="TOPIC_${data_by_topics[i].topic}" class="fw-bold"><h4>${data_by_topics[i].topic}</h4></div>
+                    <!--Content/Sub Topics-->
+                    <ul class="list-group list-group-flush">
+                        <li id="TOPIC_${data_by_topics[i].topic}_NO_SUBTOPIC" class="list-group-item"> <!-- li-elem for treasures without subtopic-->
+                            <div class="row">`
+        
+        // main-topic entries
+        for (var j in data_by_topics[i].entries) {
+            html += data_by_topics[i].entries[j]
+        }
+        html += 
+        `           </div>
+                </li>
+            </ul>
+        </li>`; // close main topic entries section
+
+        //subtopics
+        for (var j in data_by_topics[i].subtopics[0]) {
+            // subtopic (upper)
+            html += 
+                `<li class="list-group-item border-dark"><!-- li-elem for treasures with subtopic #TOPIC-#SUBTOPIC-->
+                    <div id="SUBTOPIC_${data_by_topics[i].subtopics[0][j].subtopic}" class="fw-bold"><h5>${data_by_topics[i].topic} / ${data_by_topics[i].subtopics[0][j].subtopic}</h5></div>
+                        <div class="row">`
+            // subtopic (content)
+            for (var k in data_by_topics[i].subtopics[0][j].entries) {
+                html += data_by_topics[i].subtopics[0][j].entries[k]
+            }
+            
+            // subtopic (lower)
+            html += "</div></li>"; // close subtopic entries section
+        }
+
+        // main-topic (lower)
+        html += "</ul><br>"
+    }
+    html += html_post; // lower part of the page, static
+
+    //validate HTML
+    await validate_HTML(html);
+    
+    //prettify HTML
+    console.log("Prettifying HTML...")
+    var html_prettified = prettify(html)
+
+    //write index.html to app dir
+    writeHtml(html_prettified)
 }
 
 //generates a card elem
 function generateEntryHtml (data, isNewEntry) {
-    //console.log(data, isNewEntry)
-
     //protoypes
     var new_badge = `<span class="position-absolute top-0 start-50 translate-middle badge rounded-pill bg-danger" style="font-size: 100%;">new</span>`
         if (isNewEntry == false) {
@@ -369,9 +549,9 @@ function generateEntryHtml (data, isNewEntry) {
     
     var html = 
         `<div class="col-lg-4 col-md-6 col-sm-12 mb-3 mt-3" id="${data.title}">
-            <div class="card">
+            <div class="card shadow">
                 ${new_badge}
-                <img class="card-img-top" src="${headerImage}"></img>
+                <img class="card-img-top" src="${headerImage}">
                 <div class="card-body">
                     <p class="card-text"> <!--TYPE-->
                         <span class="badge rounded-pill text-bg-secondary">
@@ -390,7 +570,7 @@ function generateEntryHtml (data, isNewEntry) {
                 </ul>
                 <div class="card-body"> <!--MAIN LINK-->
                     <a class="btn btn-primary" style="background-color: ${getRessourcesAttribute(data.ressources[0].name, "color")} !important;  border-color: ${getRessourcesAttribute(data.ressources[0].name, "color")} !important;" onmouseover="this.style.filter='brightness(80%)';" onmouseout="this.style.filter='brightness(100%)';" href="${data.ressources[0].payload}" target="_blank"><i class="${getRessourcesAttribute(data.ressources[0].name, "symbol")}"></i> ${getRessourcesAttribute(data.ressources[0].name, "publicname")}</a>
-                    </br>
+                    <br>
                     ${more_ressources}
                     <p class="card-text text-secondary pt-2" style="font-size: 85%;">
                         added: ${date}
@@ -405,7 +585,7 @@ function generateEntryHtml (data, isNewEntry) {
 
 function createCardHeaderImage (seed) {
         // Create a canvas with the same dimensions
-        const canvas = createCanvas(1600, 900);
+        const canvas = createCanvas(1600, 300);
         const ctx = canvas.getContext('2d');
     
         // Set the background color using determine_color function
@@ -437,6 +617,65 @@ function getRessourcesAttribute(name, attribute) {
         return resource[attribute];
     } else {
         return `Resource with name "${name}" or attribute "${attribute}" not found.`;
+    }
+}
+
+function getTopicsAndSubtopics() { // get topics and subtopics from topics.jsom and parse them into an array of objects -> {topic: TOPIC, entries: [], subtopics: [{subtopic: SUBTOPIC, entries: []},{subtopic: SUBTOPIC, entries: []}]}
+    const dataPath = path.join(DATA_PATH, "topics.json")
+    let data = JSON.parse(fs.readFileSync(dataPath, 'utf-8'));
+    data = data.topics
+
+    let topics = [];
+    for (var i in data) {
+        var subtopics = [];
+        for (var j in data[i].subtopics) {
+            subtopics.push({subtopic: data[i].subtopics[j], entries: []})
+        }
+        topics.push({topic: data[i].maintopic, entries: [], subtopics: [subtopics]})
+    }
+
+    // Sorting topics by topic name
+    topics.sort((a, b) => a.topic.localeCompare(b.topic));
+
+    // Sorting subtopics within each topic
+    topics.forEach(topic => {
+        topic.subtopics[0].sort((a, b) => a.subtopic.localeCompare(b.subtopic));
+    });
+
+    return topics;
+}
+
+function writeHtml(htmlContent) {
+    console.log("Writing HTML File...")
+    const outputName = "index.html"
+    const location = path.join(WEBSITE_PATH, outputName);
+    
+    fs.writeFileSync(location, htmlContent, 'utf8');
+    console.log(`HTML file has been saved as ${outputName}`);
+}
+
+async function validate_HTML(html) {
+    console.log("Validating HTML...")
+    const htmlvalidate = new HtmlValidate({
+        extends: ["html-validate:recommended"],
+        rules: {
+          "no-inline-style": "off",
+          "wcag/h37": "off", //don't check for alt elems
+          "no-trailing-whitespace": "off",
+          "no-dup-id": "off", //maybe change later
+        },
+    });
+    const report = await htmlvalidate.validateString(html);
+
+    // if html is invalid, display errors and warnings
+    if(report.valid) {
+        console.log("HMTL is valid!")
+    } else {
+        console.warn("HMTL is NOT valid!")
+        console.log(`Found ${report.results[0].errorCount} error(s) and ${report.results[0].warningCount} warning(s)`)
+        for (var i in report.results[0].messages) {
+            console.log(`ln ${report.results[0].messages[i].line} (${report.results[0].messages[i].column}): ${report.results[0].messages[i].message}    at: ${report.results[0].messages[i].selector}    rule: ${report.results[0].messages[i].ruleId}`)
+        }
     }
 }
 
